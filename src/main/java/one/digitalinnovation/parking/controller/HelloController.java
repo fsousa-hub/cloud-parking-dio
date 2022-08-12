bbf69c1a90ddc286b08ae05fed486c365f63e09d
fsousa <onics.aulas@gmail.com>
2022-08-12 20:08:45 -0300

    Estrutura inicial

---
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.parking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @RequestMapping
+    public String hello(){
+        return "Hello DIO ...";
+    }
+
+}
