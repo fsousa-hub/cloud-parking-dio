a7f1638443259027efb610947867b68c39e7a5f1
fsousa <onics.aulas@gmail.com>
2022-08-13 19:13:33 -0300

    Adicionando o Swagger

--- src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
+++ src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
@@ -1,7 +1,9 @@
 package one.digitalinnovation.parking;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CloudParkingApplication {
--- src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -1,5 +1,7 @@
 package one.digitalinnovation.parking.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import one.digitalinnovation.parking.controller.dto.ParkingCreateDTO;
 import one.digitalinnovation.parking.controller.dto.ParkingDTO;
 import one.digitalinnovation.parking.controller.mapper.ParkingMapper;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
     private final ParkingService parkingService;
@@ -25,6 +28,7 @@ public class ParkingController {
 
     // trocar List por um ResponseEntity
     @GetMapping
+    @ApiOperation("Find all parkings")
     public ResponseEntity<List<ParkingDTO>> findAll() {
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
