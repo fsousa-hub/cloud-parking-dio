7a4d028781a94444eecfd881c6dadfd3a2320761
fsousa <onics.aulas@gmail.com>
2022-08-13 15:39:50 -0300

    Update Primeiro endpoint de Lista

--- src/main/java/one/digitalinnovation/parking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.parking.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
 
-    @RequestMapping
+    @GetMapping
     public String hello(){
-        return "Hello DIO ...";
+        return "Hello DIO. Java Devs ...";
     }
 
 }
